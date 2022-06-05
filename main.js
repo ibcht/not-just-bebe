96be04267eaca66329bfb98f4559b947972a1b18
ibcht <irenee.bauchet@yahoo.fr>
2022-06-05 23:51:59 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        not just bébé
+// @namespace   https://github.com/ibcht/not-just-bebe
+// @match       https://*/*
+// @grant       none
+// @version     1.0
+// @author      ibcht
+// @description put a real name, not just bébé
+// @downloadURL  
+// @updateURL    
+// ==/UserScript==
+
+var elts = ['p','h1','h2','h3','h4','a'];
+
+elts.forEach(elt => {
+  document.querySelectorAll(elt).forEach(e => {
+    e.innerHTML = e.innerHTML
+        .replaceAll(/(un |votre |le |)bébé/ig,'Name');
+  })
+})
